395885e807f4d275fab51b1eb8aa2c49bd8624a5
Maja S Bratseth <mbratset@cisco.com>
2013-06-12 14:34:30 +0200

    AnimatorSet listeners

--- src/main/java/org/robolectric/shadows/ShadowAnimatorSet.java
+++ src/main/java/org/robolectric/shadows/ShadowAnimatorSet.java
@@ -1,13 +1,14 @@
 package org.robolectric.shadows;
 
 import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.animation.AnimatorSet;
 import android.animation.TimeInterpolator;
 import org.robolectric.annotation.Implementation;
 import org.robolectric.annotation.Implements;
 import org.robolectric.annotation.RealObject;
 
-@Implements(AnimatorSet.class)
+@Implements(value = AnimatorSet.class, inheritImplementationMethods = true)
 public class ShadowAnimatorSet extends ShadowAnimator {
   @RealObject
   private AnimatorSet realObject;
@@ -22,6 +23,24 @@ public class ShadowAnimatorSet extends ShadowAnimator {
   public void start() {
     for (Animator childAnimator : childAnimators) {
       childAnimator.setDuration(duration);
+        childAnimator.addListener(new Animator.AnimatorListener() {
+            @Override
+            public void onAnimationCancel(Animator animation) {
+                ShadowAnimatorSet.this.notifyCancel();
+            }
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                ShadowAnimatorSet.this.notifyEnd();
+            }
+            @Override
+            public void onAnimationRepeat(Animator animation) {
+                ShadowAnimatorSet.this.notifyRepeat();
+            }
+            @Override
+            public void onAnimationStart(Animator animation) {
+                ShadowAnimatorSet.this.notifyStart();
+            }
+        });
       childAnimator.start();
     }
   }
